be2ab4cff35b3abd22b766806a952220b3f268f3
malodetz <malo.detz@yandex.ru>
2024-03-24 21:30:28 +0300

    HW 5.1 Fixed

--- HW5/task1.py
+++ HW5/task1.py
@@ -3,34 +3,29 @@ import asyncio
 import os
 
 
-async def download_image(session, url, filename):
-    async with session.get(url) as response:
+async def download_image(session, filename):
+    async with session.get(url="https://source.unsplash.com/random") as response:
         if response.status == 200:
             with open(filename, "wb") as f:
-                while True:
-                    chunk = await response.content.read(1024)
-                    if not chunk:
-                        break
-                    f.write(chunk)
-            print(f"Downloaded: {filename}")
+                f.write(await response.content.read())
+            print("Downloaded:", filename)
         else:
-            print(f"Failed to download: {filename}")
+            print("Failed to download:", filename)
 
 
 async def download_all(num_images, folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
 
     async with aiohttp.ClientSession() as session:
         tasks = []
         for i in range(num_images):
-            url = "https://source.unsplash.com/random"
             filename = os.path.join(folder, f"image_{i}.jpg")
-            tasks.append(download_image(session, url, filename))
+            tasks.append(download_image(session, filename))
         await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    num_images = int(input("Enter number of images to download:"))
+    num_images = int(input("Enter number of images to download: "))
     folder = "artifacts"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     asyncio.run(download_all(num_images, folder))
